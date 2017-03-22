b38dd84eba29222a12aa3eb0062b4f14447627e6
tbouchnafa <thami.bouchnafa@1und1.de>
2017-03-22 15:15:14 +0100

    Add functionality and more

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -1,34 +1,40 @@
 import 'angular-animate';
 import 'angular-meteor';
+import 'angular-moment';
 import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
 import Angular from 'angular';
 import Loader from 'angular-ecmascript/module-loader';
 import ChatsCtrl from '../controllers/chats.controller';
+import ChatCtrl from '../controllers/chat.controller';
+import InputDirective from '../directives/input.directive';
 import CalendarFilter from '../filters/calendar.filter';
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import RoutesConfig from '../routes';
 
 const App = 'Whatsapp';
 
 Angular.module(App, [
-  'angular-meteor',
-  'ionic'
+    'angular-meteor',
+    'angularMoment',
+    'ionic'
 ]);
 
 new Loader(App)
-  .load(ChatsCtrl)
-  .load(CalendarFilter)
-  .load(RoutesConfig);
+    .load(ChatsCtrl)
+    .load(ChatCtrl)
+    .load(CalendarFilter)
+    .load(InputDirective)
+    .load(RoutesConfig);
 
 if (Meteor.isCordova) {
-  Angular.element(document).on('deviceready', onReady);
+    Angular.element(document).on('deviceready', onReady);
 }
 else {
-  Angular.element(document).ready(onReady);
+    Angular.element(document).ready(onReady);
 }
 
 function onReady() {
-  Angular.bootstrap(document, [App]);
+    Angular.bootstrap(document, [App]);
 }
